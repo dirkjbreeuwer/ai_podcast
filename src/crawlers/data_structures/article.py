8ae6285cc81181aa586fe39345b74ae3f6a852fc
Dirk Breeuwer <87372550+dirkjbreeuwer@users.noreply.github.com>
2023-09-22 07:27:44 +0000

    [Crawlers] Add article data class

---
+++ src/crawlers/data_structures/article.py
@@ -0,0 +1,68 @@
+"""
+This module defines the data structure for representing an article scraped from the web.
+"""
+from typing import List, Dict, Optional
+
+
+# pylint: disable=R0902
+class Article:
+    """
+    Represents an article scraped from the web.
+
+    Attributes:
+        url (Optional[str]): The URL of the article.
+        loaded_domain (Optional[str]): The domain from which the article was loaded.
+        title (str): The title of the article.
+        date (str): The publication date of the article.
+        author (Optional[List[str]]): The authors of the article.
+        description (Optional[str]): A brief description or summary of the article.
+        keywords (Optional[str]): Keywords associated with the article.
+        lang (Optional[str]): The language of the article.
+        tags (Optional[List[str]]): Tags associated with the article.
+        image (Optional[str]): The main image URL of the article.
+        text (str): The main text content of the article.
+        id (str): A unique identifier for the article.
+    """
+
+    # Disabling the too many arguments warning because we want Article
+    # to be a comprehensive data model
+    # pylint: disable=R0913
+    def __init__(
+        self,
+        title: str,
+        text: str,
+        _id: str,
+        date: str,
+        url: Optional[str] = None,
+        loaded_domain: Optional[str] = None,
+        author: Optional[List[str]] = None,
+        description: Optional[str] = None,
+        keywords: Optional[str] = None,
+        lang: Optional[str] = None,
+        tags: Optional[List[str]] = None,
+        image: Optional[str] = None,
+        videos: Optional[List[Dict[str, str]]] = None,
+    ):
+        """Initializes an Article instance with provided attributes."""
+        self.url = url
+        self.loaded_domain = loaded_domain
+        self.title = title
+        self.date = date
+        self.author = author
+        self.description = description
+        self.keywords = keywords
+        self.lang = lang
+        self.tags = tags
+        self.image = image
+        self.videos = videos
+        self.text = text
+        self._id = _id
+
+    def __repr__(self):
+        """Returns a string representation of the Article instance."""
+        return f"<Article(title={self.title}, url={self.url}, date={self.date})>"
+
+    def get_summary(self) -> str:
+        """Returns a summary of the article (this is just a placeholder
+        and should be implemented properly)"""
+        return self.text[:100] + "..."
