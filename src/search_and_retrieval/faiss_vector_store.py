22f1ed13b2695117cad6589b522d454deaec98f2
Dirk Breeuwer <87372550+dirkjbreeuwer@users.noreply.github.com>
2023-09-24 10:39:17 +0000

    [Search] Implement search engine and extend FAISS to implement search engine too

--- src/search_and_retrieval/faiss_vector_store.py
+++ src/search_and_retrieval/faiss_vector_store.py
@@ -21,9 +21,10 @@ from typing import List, Tuple, Dict, Optional
 from langchain.vectorstores.faiss import FAISS
 from langchain.schema.embeddings import Embeddings
 from .vector_store import VectorStore
+from .search_engine import SearchEngine
 
 
-class FAISSStore(VectorStore):
+class FAISSStore(VectorStore, SearchEngine):
     """FAISS implementation of the VectorStore using langchain wrapper."""
 
     def __init__(self, embeddings: Embeddings):
@@ -63,3 +64,17 @@ class FAISSStore(VectorStore):
     def load_index(self, file_path: str) -> None:
         # Load the FAISS index from the specified file path
         self.index = FAISS.load_local(file_path)
+
+    def convert_query_to_vector(self, query: str) -> List[float]:
+        return self.embeddings.embed_query(query)
+
+    def execute_similarity_search(
+        self, query_vector: List[float]
+    ) -> List[Tuple[int, float]]:
+        return self.similarity_search(query_vector)
+
+    def execute_advanced_search(
+        self, query_vector: List[float], metadata: dict
+    ) -> List[Tuple[int, float]]:
+        # Implement advanced search combining vector similarity with metadata filtering
+        pass
